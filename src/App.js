8bca21133ca0079c0929621bba1bc1adfc5bc5e2
ayushmanbh <ashbh.dhp14@gmail.com>
2020-07-04 12:47:31 +0530

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+// import Pagination from './components/ui/Pagination'
+import Pagination from 'react-js-pagination'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+  const [activePage, setActivePage] = useState(1)
+  const [itemsPerPage] = useState(8)
+
+  const handlePageChange = (pageNumber) => setActivePage(pageNumber)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters/?name=${query}`)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
+  const indexOfLastItem = activePage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={currentItems} />
+      <div className='d-flex justify-content-center mt-3'>
+        <Pagination
+          activePage={activePage}
+          itemsCountPerPage={itemsPerPage}
+          totalItemsCount={items.length}
+          pageRangeDisplayed={5}
+          onChange={handlePageChange}
+          itemClass="page-item"
+          linkClass="page-link"
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+// <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
+
+export default App
---
+++ src/components/ui/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+
+const Header = () => (
+  <header className='center'>
+    <img src={logo} alt=''></img>
+  </header>
+)
+
+export default Header
